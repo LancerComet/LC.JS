106bba835cbf883b95cdfc4f6ccd5540bc22e6c5
LancerComet <chw644@hotmail.com>
2017-11-07 01:43:50 +0800

    + [WIP] Testing case.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * LC.JS is a simple, flex and strict UI library that for TypeScripters.
+ * For more detail, please check https://github.com/LancerComet/LC.JS
+ *
+ * @author LancerComet
+ * @copyright LancerComet
+ * @license MIT
+ */
+
 import { LC } from './core/lc'
 import { Component } from './component'
 import { createDirective } from './directives'
--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -1,6 +1,17 @@
+import { evaluateExpression } from './util.evalutation'
 import { getAllProperties } from './'
+import { isArray } from './util.is-array'
+import { isFunction } from './util.is-func'
 
 describe('Utils testing.', () => {
+  it('Should make evalutation correctly.', () => {
+    chai.expect(
+      evaluateExpression(['name', 'age'], ['wch', 100], '"I\'m " + name + " and I\'m " + age + "-year old."')
+    ).to.equals('I\'m wch and I\'m 100-year old.')
+
+    chai.expect(evaluateExpression(['name'], ['wch'], 'age')).to.equals('')
+  })
+
   it('Should get all properties.', () => {
     chai.expect(getAllProperties({})).to
       .include('__proto__').and
@@ -17,5 +28,19 @@ describe('Utils testing.', () => {
     chai.expect(getAllProperties(new B())).to.contain('a').and.contain('b')
   })
 
-  it('Should know whether a variable is an array.')
+  it('Should know whether some variable is an array.', () => {
+    chai.expect(isArray([])).to.equals(true)
+    chai.expect(isArray({})).to.equals(false)
+    chai.expect(isArray(10)).to.equals(false)
+    chai.expect(isArray('')).to.equals(false)
+    chai.expect(isArray(() => {})).to.equals(false)
+  })
+
+  it('Should konw whether some variable is a function.', () => {
+    chai.expect(isFunction(() => {})).to.equals(true)
+    chai.expect(isFunction(1)).to.equals(false)
+    chai.expect(isFunction('')).to.equals(false)
+    chai.expect(isFunction({})).to.equals(false)
+    chai.expect(isFunction([])).to.equals(false)
+  })
 })
